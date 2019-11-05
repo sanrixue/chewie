87209b717dfcc4d6a834bce03372fe796dff14a4
Eureka <1040811211@qq.com>
2019-11-05 13:54:48 +0800

    add:chewie

---
+++ chewie-0.9.8+1/lib/src/player_with_controls.dart
@@ -0,0 +1,81 @@
+import 'dart:ui';
+
+import 'package:chewie/src/chewie_player.dart';
+import 'package:chewie/src/cupertino_controls.dart';
+// import 'package:chewie/src/material_controls.dart';
+import 'package:engin/ViewModel/FormalMaterial_controls.dart';
+import 'package:engin/util/ImportUtil.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class PlayerWithControls extends StatelessWidget {
+  int indexChapter;
+  int indexSection;
+  List<Item> beanItemList;
+  PlayerWithControls({Key key,
+    this.indexChapter,
+    this.indexSection,
+    this.beanItemList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ChewieController chewieController = ChewieController.of(context);
+
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: AspectRatio(
+          aspectRatio:
+              chewieController.aspectRatio ?? _calculateAspectRatio(context),
+          child: _buildPlayerWithControls(chewieController, context),
+        ),
+      ),
+    );
+  }
+
+  Container _buildPlayerWithControls(
+      ChewieController chewieController, BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          chewieController.placeholder ?? Container(),
+          Center(
+            child: AspectRatio(
+              aspectRatio: chewieController.aspectRatio ??
+                  _calculateAspectRatio(context),
+              child: VideoPlayer(chewieController.videoPlayerController),
+            ),
+          ),
+          chewieController.overlay ?? Container(),
+          _buildControls(context, chewieController),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControls(
+    BuildContext context,
+    ChewieController chewieController,
+  ) {
+    return chewieController.showControls
+        ? chewieController.customControls != null
+            ? chewieController.customControls
+            : Theme.of(context).platform == TargetPlatform.android
+                ? FormalMaterialControls(indexChapter: this.indexChapter,indexSection: this.indexSection,beanItemList: this.beanItemList,)
+                : CupertinoControls(
+                    backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
+                    iconColor: Color.fromARGB(255, 200, 200, 200),
+                  )
+        : Container();
+  }
+
+  double _calculateAspectRatio(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    return width > height ? width / height : height / width;
+  }
+}
